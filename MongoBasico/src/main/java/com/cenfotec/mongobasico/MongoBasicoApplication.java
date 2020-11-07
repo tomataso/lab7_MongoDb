0ba1a7395fe89df13a9857335e1602ac6a97dd92
tomataso <tomataso@users.noreply.github.com>
2020-11-07 17:43:52 -0600

    otro fix con el import
    (el import es de slf4j) en el main

--- MongoBasico/src/main/java/com/cenfotec/mongobasico/MongoBasicoApplication.java
+++ MongoBasico/src/main/java/com/cenfotec/mongobasico/MongoBasicoApplication.java
@@ -1,7 +1,8 @@
 package com.cenfotec.mongobasico;
 
 import org.springframework.boot.SpringApplication;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
