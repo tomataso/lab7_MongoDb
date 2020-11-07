50e2a3bfe6a77f11eef8fa6ea52179e62082485b
tomataso <tomataso@users.noreply.github.com>
2020-11-07 17:31:04 -0600

    Fix Errores Parte A

--- MongoBasico/src/main/java/com/cenfotec/mongobasico/repository/JournalRepository.java
+++ MongoBasico/src/main/java/com/cenfotec/mongobasico/repository/JournalRepository.java
@@ -1,5 +1,12 @@
 package com.cenfotec.mongobasico.repository;
 
-public interface JournalRepository {
+import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.cenfotec.mongobasico.domain.Journal;
+
+
+public interface JournalRepository extends MongoRepository<Journal, String> {
+public List<Journal> findByTitleLike(String word);
 }
