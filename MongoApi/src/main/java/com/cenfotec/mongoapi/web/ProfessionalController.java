2b2159acf66f5de1a1e795c2d481c84d410a8317
tomataso <tomataso@users.noreply.github.com>
2020-11-07 18:10:57 -0600

    lab 7 terminado

---
+++ MongoApi/src/main/java/com/cenfotec/mongoapi/web/ProfessionalController.java
@@ -0,0 +1,23 @@
+package com.cenfotec.mongoapi.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cenfotec.mongoapi.domain.Professional;
+import com.cenfotec.mongoapi.service.ProfessionalService;
+
+@RestController
+public class ProfessionalController {
+	@Autowired
+	private ProfessionalService professionalService;
+	
+	@PostMapping("/professional")
+	public ResponseEntity<String> addColleague(@RequestBody Professional professional) {
+	professionalService.saveProfessional(professional);
+	return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+}
